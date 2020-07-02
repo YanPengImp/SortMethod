0464590f9c6303fd2619b5b5c9f20432ccd7a79c
imp <464553111@qq.com>
2020-07-02 16:13:37 +0800

    add merge sort

--- QuickSort/ViewController.m
+++ QuickSort/ViewController.m
@@ -23,14 +23,16 @@
 //    selectSort(a, count);
 //    insertSort(a, count);
 //    sortArr(a, 0, count - 1);
-    shellSort(a,count);
+//    shellSort(a,count);
+    int *b = mergeSort(a, count);
     for (int i = 0; i < count; i++) {
-        printf("%d\n", a[i]);
+        printf("%d\n", b[i]);
     }
     // Do any additional setup after loading the view, typically from a nib.
 }
 
 //冒泡排序
+//两两比较交换 O(n*n)
 void bubblingSort(int array[],int count) {
     for (int i = 0; i < count - 1; i++) {
         for (int j = 0; j < count - 1 - i; j++) {
@@ -44,6 +46,7 @@ void bubblingSort(int array[],int count) {
 }
 
 //选择排序
+//每一次找到最小值，然后和第一个数交换 O(n*n)
 void selectSort(int array[],int count) {
     for (int i = 0; i < count - 1; i++) {
         int minIndex = i;
@@ -61,6 +64,7 @@ void selectSort(int array[],int count) {
 }
 
 //插入排序
+//前面都是有序 后面无序，遍历如果比之前有序的最后一位小，就依次把前面的有序往后移动一位。 O(n*n)
 void insertSort(int array[],int count) {
     for (int i = 0; i < count; i++) {
         int j = i;
@@ -74,6 +78,7 @@ void insertSort(int array[],int count) {
 }
 
 //快速排序
+//找到中间index位于左边的都小 右边的都大，再对左右序列分别排序 O(n*logn)
 void sortArr(int array[], int left, int right) {
     if (left >= right) {
         return;
@@ -100,6 +105,7 @@ int quickSort(int array[], int left, int right) {
 }
 
 //希尔排序
+//每次把序列分成间隔count/2的子序列 分别进行插入排序。直到count=1
 void shellSort(int array[], int count) {
     int interval = count / 2;
     while (interval > 0) {
@@ -114,6 +120,38 @@ void shellSort(int array[], int count) {
         }
         interval /= 2;
     }
+}
+
+//归并排序
+int* mergeSort(int array[], int count) {
+    int length = count / 2;
+    int leftArr[length];
+    int rightArr[count - length];
+    for (int i = 0; i < count; i++) {
+        if (i < length) {
+            leftArr[i] = array[i];
+        } else {
+            rightArr[i-length] = array[i];
+        }
+    }
+    return merge(mergeSort(leftArr, length), length, mergeSort(rightArr, count - length), count - length);
+}
+
+int* merge(int letfArr[], int lefLength, int rightArr[], int rightLength) {
+    int i = 0, j = 0;
+    int res[lefLength+rightLength];
+    for (int index = 0; index < lefLength+rightLength; index++) {
+        if (index>i) {
+            res[index] = rightArr[j++];
+        } else if (index>j) {
+            res[index] = letfArr[i++];
+        } else if (letfArr[i]<rightArr[j]) {
+            res[index] = letfArr[i++];
+        } else {
+            res[index] = rightArr[j++];
+        }
+    }
+    return res;
 }
 
 - (void)didReceiveMemoryWarning {
